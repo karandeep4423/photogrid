577f111dc0c6fbdd9663a0c6c6aa952e596e1cf6
karandeep4423 <karanhanju9696@gmail.com>
2024-01-31 00:37:32 +0100

    sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,92 +1,91 @@
-
-// export default async function sitemap(){
-//     const baseUrl = "https://photo-grid.org";
+export default async function sitemap(){
+    const baseUrl = "https://photo-grid.org";
   
-//     const res = await fetch("http://localhost:3000/api/images", {
-//       method: "GET",
-//     });
+    const res = await fetch("/api/images", {
+      method: "GET",
+    });
   
-//     const result = await res.json();
+    const result = await res.json();
   
-//     const uniqueEntries: any[] = [];
+    const uniqueEntries: any[] = [];
   
-//     result.forEach((img: any) => {
-//       const imageEntry = {
-//         url: `${baseUrl}/${img.imageName}`,
-//         lastModified: new Date(),
-//       };
+    result.forEach((img: any) => {
+      const imageEntry = {
+        url: `${baseUrl}/${img.imageName}`,
+        lastModified: new Date(),
+      };
   
-//       const categoryEntry = {
-//         url: `${baseUrl}/${img.imageCategory}`,
-//         lastModified: new Date(),
-//       };
+      const categoryEntry = {
+        url: `${baseUrl}/${img.imageCategory}`,
+        lastModified: new Date(),
+      };
   
-//       if (!uniqueEntries.some((entry) => entry.url === imageEntry.url)) {
-//         uniqueEntries.push(imageEntry);
-//       }
+      if (!uniqueEntries.some((entry) => entry.url === imageEntry.url)) {
+        uniqueEntries.push(imageEntry);
+      }
   
-//       if (!uniqueEntries.some((entry) => entry.url === categoryEntry.url)) {
-//         uniqueEntries.push(categoryEntry);
-//       }
-//     });
+      if (!uniqueEntries.some((entry) => entry.url === categoryEntry.url)) {
+        uniqueEntries.push(categoryEntry);
+      }
+    })??[];
   
-//     const sitemapEntries = [
-//       ...uniqueEntries,
-//       {
-//         url: baseUrl,
-//         lastModified: new Date(),
-//       },
-//     ];
+    const sitemapEntries = [
+      ...uniqueEntries,
+      {
+        url: baseUrl,
+        lastModified: new Date(),
+      },
+    ];
   
-//     return sitemapEntries;
-//   }
+    return sitemapEntries;
+  }
   
-import { MetadataRoute } from 'next';
+// import { MetadataRoute } from 'next';
 
-export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
-  return new Promise(async (resolve, reject) => {
-    try {
-      const baseUrl = "https://photo-grid.org";
+// export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+//   return new Promise(async (resolve, reject) => {
+//     try {
+//       const baseUrl = "https://photo-grid.org";
 
-      const res = await fetch("/api/images", {
-        method: "GET",
-      });
+//       const res = await fetch("/api/images", {
+//         method: "GET",
+//       });
 
-      const result = await res.json();
+//       const result = await res.json();
 
-      const uniqueEntries: any[] = [];
+//       const uniqueEntries: any[] = [];
 
-      result.forEach((img: any) => {
-        const imageEntry = {
-          url: `${baseUrl}/${img.imageName}`,
-          lastModified: new Date(),
-        };
+//       result.forEach((img: any) => {
+//         const imageEntry = {
+//           url: `${baseUrl}/${img.imageName}`,
+//           lastModified: new Date(),
+//         };
 
-        const categoryEntry = {
-          url: `${baseUrl}/${img.imageCategory}`,
-          lastModified: new Date(),
-        };
+//         const categoryEntry = {
+//           url: `${baseUrl}/${img.imageCategory}`,
+//           lastModified: new Date(),
+//         };
 
-        if (!uniqueEntries.some((entry) => entry.url === imageEntry.url)) {
-          uniqueEntries.push(imageEntry);
-        }
+//         if (!uniqueEntries.some((entry) => entry.url === imageEntry.url)) {
+//           uniqueEntries.push(imageEntry);
+//         }
 
-        if (!uniqueEntries.some((entry) => entry.url === categoryEntry.url)) {
-          uniqueEntries.push(categoryEntry);
-        }
-      });
+//         if (!uniqueEntries.some((entry) => entry.url === categoryEntry.url)) {
+//           uniqueEntries.push(categoryEntry);
+//         }
+//       });
 
-      const sitemapEntries = [
-        ...uniqueEntries,
-        {
-          url: baseUrl,
-          lastModified: new Date(),
-        },
-      ];
+//       const sitemapEntries = [
+//         ...uniqueEntries,
+//         {
+//           url: baseUrl,
+//           lastModified: new Date(),
+//         },
+//       ];
 
-      resolve(sitemapEntries);
-    } catch (error) {
-      reject(error);
-    }
-  });
-}
+//       resolve(sitemapEntries);
+//     } catch (error) {
+//       reject(error);
+//     }
+//   });
+// }
