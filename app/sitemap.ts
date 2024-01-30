0e4f324f43b8e704b5af65e5a9dd4462b5351d16
karandeep4423 <karanhanju9696@gmail.com>
2024-01-30 23:09:12 +0100

    url sitemp

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,42 +1,48 @@
 export default async function sitemap() {
-    const baseUrl = "https://photo-grid.org";
-  
+  const baseUrl = "https://photo-grid.org";
+
+  const isPrerendering = typeof window === "undefined";
+
+  let result = [];
+
+  if (!isPrerendering) {
+    // Fetch data only during runtime
     const res = await fetch("/api/images", {
       method: "GET",
     });
-  
-    const result = await res.json();
-  
-    const uniqueEntries: any[] = [];
-  
-    result.forEach((img: any) => {
-      const imageEntry = {
-        url: `${baseUrl}/${img.imageName}`,
-        lastModified: new Date(),
-      };
-  
-      const categoryEntry = {
-        url: `${baseUrl}/${img.imageCategory}`,
-        lastModified: new Date(),
-      };
-  
-      if (!uniqueEntries.some((entry) => entry.url === imageEntry.url)) {
-        uniqueEntries.push(imageEntry);
-      }
-  
-      if (!uniqueEntries.some((entry) => entry.url === categoryEntry.url)) {
-        uniqueEntries.push(categoryEntry);
-      }
-    });
-  
-    const sitemapEntries = [
-      ...uniqueEntries,
-      {
-        url: baseUrl,
-        lastModified: new Date(),
-      },
-    ];
-  
-    return sitemapEntries;
+
+    result = await res.json();
   }
-  
+
+  const uniqueEntries: any[] = [];
+
+  result.forEach((img: any) => {
+    const imageEntry = {
+      url: `${baseUrl}/${img.imageName}`,
+      lastModified: new Date(),
+    };
+
+    const categoryEntry = {
+      url: `${baseUrl}/${img.imageCategory}`,
+      lastModified: new Date(),
+    };
+
+    if (!uniqueEntries.some((entry) => entry.url === imageEntry.url)) {
+      uniqueEntries.push(imageEntry);
+    }
+
+    if (!uniqueEntries.some((entry) => entry.url === categoryEntry.url)) {
+      uniqueEntries.push(categoryEntry);
+    }
+  });
+
+  const sitemapEntries = [
+    ...uniqueEntries,
+    {
+      url: baseUrl,
+      lastModified: new Date(),
+    },
+  ];
+
+  return sitemapEntries;
+}
