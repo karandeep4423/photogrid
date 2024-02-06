36aca28681dac52defc97a996caeba76d02d97af
karandeep4423 <karanhanju9696@gmail.com>
2024-02-06 22:06:32 +0100

    rsolving err

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,41 +1,41 @@
-export default async function sitemap(){
-    const baseUrl = "https://photo-grid.org";
+// export default async function sitemap(){
+//     const baseUrl = "https://photo-grid.org";
   
-    const res = await fetch("https://photo-grid.org/api/images", {
-      method: "GET",
-    });
+//     const res = await fetch("https://photo-grid.org/api/images", {
+//       method: "GET",
+//     });
   
-    const result = await res.json();
+//     const result = await res.json();
   
-    const uniqueEntries: any[] = [];
+//     const uniqueEntries: any[] = [];
   
-    result.forEach((img: any) => {
-      const imageEntry = {
-        url: `${baseUrl}/${img.imageName}`,
-        lastModified: new Date(),
-      };
+//     result.forEach((img: any) => {
+//       const imageEntry = {
+//         url: `${baseUrl}/${img.imageName}`,
+//         lastModified: new Date(),
+//       };
   
-      const categoryEntry = {
-        url: `${baseUrl}/${img.imageCategory}`,
-        lastModified: new Date(),
-      };
+//       const categoryEntry = {
+//         url: `${baseUrl}/${img.imageCategory}`,
+//         lastModified: new Date(),
+//       };
   
-      if (!uniqueEntries.some((entry) => entry.url === imageEntry.url)) {
-        uniqueEntries.push(imageEntry);
-      }
+//       if (!uniqueEntries.some((entry) => entry.url === imageEntry.url)) {
+//         uniqueEntries.push(imageEntry);
+//       }
   
-      if (!uniqueEntries.some((entry) => entry.url === categoryEntry.url)) {
-        uniqueEntries.push(categoryEntry);
-      }
-    })??[];
+//       if (!uniqueEntries.some((entry) => entry.url === categoryEntry.url)) {
+//         uniqueEntries.push(categoryEntry);
+//       }
+//     })??[];
   
-    const sitemapEntries = [
-      ...uniqueEntries,
-      {
-        url: baseUrl,
-        lastModified: new Date(),
-      },
-    ];
+//     const sitemapEntries = [
+//       ...uniqueEntries,
+//       {
+//         url: baseUrl,
+//         lastModified: new Date(),
+//       },
+//     ];
   
-    return sitemapEntries;
-  }
+//     return sitemapEntries;
+//   }
