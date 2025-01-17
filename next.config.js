120b7dac9385bf1116bc73ce2e9e0083444e12e1
karandeep4423 <karanhanju9696@gmail.com>
2025-01-17 12:23:07 +0100

    change next config file

--- next.config.js
+++ next.config.js
@@ -1,15 +1,11 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    images: {
-      remotePatterns: [
-        {
-          protocol: 'https',
-          hostname: 'd1zs065awsyu72.cloudfront.net',
-          // hostname: "photo-grid.org.s3.eu-west-3.amazonaws.com"
-        },
-      ],
-    },
-  };
-  
-  module.exports = nextConfig;
+module.exports = {
+  async rewrites() {
+    return [
+      {
+        source: '/:path*',
+        destination: 'https://d1zs065awsyu72.cloudfront.net/:path*'
+      }
+    ]
+  }
+}
   
